93864285b495f8aac277c2ed6f9dc9ed4148279d
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-20 21:40:21 -0400

    adding more pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Banner from "./components/Banner/Banner";
 // import NotFound from "./containers/NotFound/NotFound";
 
 function App() {
   return (
     <Router>
       <NavBar />
+      <Banner />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
