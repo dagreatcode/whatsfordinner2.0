08d3e3f4b08618c61ec60d8fca0d3621117e5f4a
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-28 13:52:45 -0400

    updates to the product page

--- client/src/components/ShopSection/pagination.jsx
+++ client/src/components/ShopSection/pagination.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Pagination() {
   return (
     <>
-      <ul className="nav justify-content-center">
+      {/* <ul className="nav justify-content-center">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to={"#"}>
             1
@@ -30,7 +30,39 @@ export default function Pagination() {
             5
           </Link>
         </li>
-      </ul>
+      </ul> */}
+      <nav aria-label="Page navigation example" style={{  /* Center horizontally*/
+  margin: "0 auto;"}}>
+        <ul className="pagination">
+          <li className="page-item">
+            <Link className="page-link" to="/" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span className="sr-only">Previous</span>
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              1
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              2
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/">
+              3
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to="/" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span className="sr-only">Next</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
