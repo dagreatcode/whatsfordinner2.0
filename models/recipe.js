a550b97aaad2969eb3d3d0dff5f13be76fba4ea5
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-29 14:20:41 -0400

    working on the db connection

--- models/recipe.js
+++ models/recipe.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const recipeSchema = new mongoose.Schema({
+const recipeSchema = new Schema({
   title: {
     type: String,
     required: true,
